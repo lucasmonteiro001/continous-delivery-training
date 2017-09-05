a6e910cc16a13bff7a69517c6df62923abfb71bd
Lucas Monteiro <lucasmonteiro001@gmail.com>
2017-09-05 18:26:44 -0300

    add pm2

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const app = express()
+const PORT = 59999;
 
 app.get('/', function (req, res) {
   res.send('Hello World!')
 })
 
-app.listen(3000, function () {
-  console.log('App listening on port 3000!')
+app.listen(PORT, function () {
+  console.log(`App listening on port ${PORT}!`)
 })
