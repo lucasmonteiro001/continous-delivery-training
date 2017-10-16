4c8b6858908943ed015b66e4cbbd9ca8ac5759e3
Lucas Monteiro <lucasmonteiro001@gmail.com>
2017-10-16 17:30:22 -0200

    excluding dir

--- ansible/scripts/sync.sh
+++ ansible/scripts/sync.sh
@@ -3,4 +3,4 @@
 USER=lmonteir
 
 # copy all root folder to aws
-rsync -Cravzp ../ lmonteir@ec2-13-59-84-172.us-east-2.compute.amazonaws.com:/home/${USER}/ansible
+rsync -Cravzp --exclude ../.vagrant ../ lmonteir@ec2-13-59-84-172.us-east-2.compute.amazonaws.com:/home/${USER}/ansible
