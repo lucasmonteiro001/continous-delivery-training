b73ca74c895121018dcc25cde773f237b3f21317
Lucas Monteiro <lucasmonteiro001@gmail.com>
2017-10-18 11:22:47 -0200

    created new provision to copy files

--- ansible/scripts/run.sh
+++ ansible/scripts/run.sh
@@ -2,3 +2,5 @@
 
 ansible-galaxy install -r ../requirements.yml --force
 ansible-playbook -i ../inventories/aws/inventory ../playbook.yml --connection=local
+
+ansible all -i ../inventories/vagrant/inventory  -a "/usr/local/lib/npm/bin/pm2 delete all"
