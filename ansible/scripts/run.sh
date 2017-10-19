538674f76618b095373fede3cc8a307c1581eba9
Lucas Monteiro <lucasmonteiro001@gmail.com>
2017-10-19 10:48:14 -0200

    spliting deploy/provision files

--- ansible/scripts/run.sh
+++ ansible/scripts/run.sh
@@ -4,3 +4,5 @@ ansible-galaxy install -r ../requirements.yml --force
 ansible-playbook -i ../inventories/aws/inventory ../playbook.yml --connection=local
 
 ansible all -i ../inventories/vagrant/inventory  -a "/usr/local/lib/npm/bin/pm2 delete all"
+
+ansible-playbook -i ../inventories/vagrant/inventory  ../deploy/deploy.yml
